223757048d3c1782bc1e898f2e80129835e36755
Tom Bruce <tombruce@outlook.com.au>
2024-07-28 13:45:51 +1000

    refactor: added factory methods for different environments to the ServiceLocator

--- src/domain/shared/service-locator.ts
+++ src/domain/shared/service-locator.ts
@@ -1,30 +1,60 @@
+import { IAuthenticationService } from "@/domain/authentication/authentication.interface";
+import { AuthenticationRepository } from "@/domain/authentication/authentication.repository";
+import { AuthenticationService } from "@/domain/authentication/authentication.service";
+import { ICookieService } from "@/domain/cookie/cookie.interface";
+import { NextjsPagesApiCookieService } from "@/domain/cookie/cookie.service";
+import { IEmailService } from "@/domain/email/email.interface";
+import { ResendEmailService } from "@/domain/email/email.service";
+import { UserRepository } from "@/domain/user/user.repository";
+import { NextApiRequest, NextApiResponse } from "next";
+
 // // Maps a service name to a service INTERFACE
 type ServiceMap = {
-  //   UserService: IUserService;
-  //     TeamService: ITeamService;
+  AuthenticationService: IAuthenticationService;
+  CookieService: ICookieService;
+  EmailService: IEmailService;
+
   //   AuthenticationService: IAuthenticationService
 };
 
 type Service = keyof ServiceMap;
 
+interface IServiceLocator {
+  getService<K extends keyof ServiceMap>(name: K): ServiceMap[K];
+}
 /**
  * The ServiceLocator class is responsible for initialising services within the application.
  */
-class ServiceLocator {
+export class ServiceLocator implements IServiceLocator {
   // Maps a service name to a concrete service IMPLEMENTATION
   // This could be improved by caching the creation of services and repositories
-  private static _serviceFactory: { [K in Service]: () => ServiceMap[K] } = {
-    // AuthenticationService: () => {
-    //     return new AuthenticationService()
-    // },
-    // UserService: () => {
-    //   const userRepo = new InMemoryUserRepository();
-    //   return new UserService(userRepo);
-    // },
-  };
+  private _serviceFactory: { [K in Service]: () => ServiceMap[K] };
+  constructor(serviceFactory: { [K in Service]: () => ServiceMap[K] }) {
+    this._serviceFactory = serviceFactory;
+  }
 
-  static getService<K extends keyof ServiceMap>(name: K): ServiceMap[K] {
-    const serviceFactory = ServiceLocator._serviceFactory[name]();
+  static createNextjsServiceLocator(req: NextApiRequest, res: NextApiResponse): ServiceLocator {
+    // TODO we should look at having a repository/service cache to reuse the same service instances
+    return new ServiceLocator({
+      EmailService: () => new ResendEmailService(),
+      AuthenticationService: () => {
+        const authenticationRepository = new AuthenticationRepository();
+        const userRepository = new UserRepository();
+        const cookieService = new NextjsPagesApiCookieService(req, res);
+        const emailService = new ResendEmailService();
+        return new AuthenticationService(
+          authenticationRepository,
+          userRepository,
+          emailService,
+          cookieService
+        );
+      },
+      CookieService: () => new NextjsPagesApiCookieService(req, res),
+    });
+  }
+
+  getService<K extends keyof ServiceMap>(name: K): ServiceMap[K] {
+    const serviceFactory = this._serviceFactory[name]();
     if (!serviceFactory) {
       throw new Error(`Service ${name} not found.`);
     }
--- src/pages/api/temp.ts
+++ src/pages/api/temp.ts
@@ -1,37 +1,15 @@
-import { AuthenticationRepository } from "@/domain/authentication/authentication.repository";
-import { AuthenticationService } from "@/domain/authentication/authentication.service";
-import { NextjsPagesApiCookieService } from "@/domain/cookie/cookie.service";
-import { NoOpEmailService } from "@/domain/email/email.service";
-import { UserRepository } from "@/domain/user/user.repository";
+import { ServiceLocator } from "@/domain/shared/service-locator";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const emailService = new NoOpEmailService();
-  const cookieService = new NextjsPagesApiCookieService(req, res);
-  const userRepository = new UserRepository();
-  const authenticationRepository = new AuthenticationRepository();
+  const locator = ServiceLocator.createNextjsServiceLocator(req, res);
+  const authenticationService = locator.getService("AuthenticationService");
 
-  const authenticationService = new AuthenticationService(
-    authenticationRepository,
-    userRepository,
-    emailService,
-    cookieService
-  );
-
-  //   const authRes = await authenticationService.signInWithEmail({
-  //     email: "tbruc3@gmail.com",
-  //     unhashedPassword: "Password",
-  //   });
-
-  //   await authenticationService.signOut();
-  //   const result = await authenticationService.signUpWithEmail({
-  //     email: "tbruc3+verify@gmail.com",
-  //     unhashedPassword: "Password",
-  //   });
-  //   const result = await authenticationService.verifyEmailWithCode({ code: "02156166" });
   const result = await authenticationService.getUser();
-  //   const result = await authenticationService.initiateEmailVerification();
-  //   const result = await authenticationService.();
+  // const result = await authenticationService.signInWithEmail({
+  //   email: "tbruc3@gmail.com",
+  //   unhashedPassword: "Password",
+  // });
   console.log({ result });
   return res.status(200).json({ message: "Hello from Next.js!" });
 }
